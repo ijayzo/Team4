bea1dc38ecd0c8fd68df42c6749327ebfbe1aa7a
qwakumuller <richmondawuahadjei@yahoo.com>
2022-02-27 15:53:36 -0500

    Try Deployment into K8,Expecting failure

--- src/main/java/com/example/demo/Exceptions/UserAlreadyExist.java
+++ src/main/java/com/example/demo/Exceptions/UserAlreadyExist.java
@@ -1,5 +1,8 @@
 package com.example.demo.Exceptions;
 
+/**
+ * throws Exception when user does not exist
+ */
 public class UserAlreadyExist extends RuntimeException{
 
     public UserAlreadyExist(){
