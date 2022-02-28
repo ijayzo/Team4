61fd279d7380866602f6cca28298b00e45c08844
ijayzo <isaias.jasso3@gmail.com>
2022-02-28 17:13:02 -0500

    most tests

--- src/test/java/HotelControllerTest.java
+++ src/test/java/HotelControllerTest.java
@@ -1,16 +1,107 @@
+
+import com.example.demo.Controllers.HotelPartnerController;
 import com.example.demo.DAO.HotelPartnerRepository;
 import com.example.demo.Models.HotelPartner;
 import com.example.demo.Services.HotelPartnerServices;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.MockBeans;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 // rely on the Spring Boot container and we want also to add or mock one of the container beans then @MockBean
-@MockBeans({@MockBean(HotelPartnerServices.class), @MockBean(HotelPartnerRepository.class) })
+@ActiveProfiles(value = "test")
+@WebMvcTest(HotelPartnerController.class)
 public class HotelControllerTest {
+    @Mock
+    private HotelPartner hotelPartner;
+    @MockBean
+    private HotelPartnerRepository hotelPartnerRepository;
+    @MockBean
+    private HotelPartnerServices hotelPartnerServices;
+    @InjectMocks
+    private HotelPartnerController hotelPartnerController;
     @Autowired
-    HotelPartner hotelPartner;
-    @Autowired
-    HotelPartnerRepository hotelPartnerRepository;
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() throws Exception{
+        mockMvc = MockMvcBuilders.standaloneSetup(hotelPartnerController)
+                .build();
+    }
+
+    private HotelPartner createHotel(){
+        HotelPartner testHotel = new HotelPartner();
+        testHotel.setId(8);
+        testHotel.setHotelName("TestHotel");
+        testHotel.setHotelLocation("NewLocation");
+        testHotel.setIsDeleted(false);
+        return testHotel;
+    }
+
+    @Test
+    public void shouldReturnModel() throws Exception{
+        int id = 10;
+        HotelPartner testHotel = new HotelPartner();
+        testHotel = createHotel();
+
+        when(hotelPartnerServices.getHotelByHotelId(10)).thenReturn(createHotel());
+
+        mockMvc.perform(get("/hotel/id/{id}", id))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(id));
+    }
+
+    @Test
+    public void shouldGetAll() throws Exception{
+        List<HotelPartner> hotelPartnerList = hotelPartnerRepository.findAll();
+        when(hotelPartnerServices.getAllHotels()).thenReturn(hotelPartnerList);
+        mockMvc.perform(get("/hotel/all"))
+                .andExpect(status().isOk());
+    }
+    //    @Test
+//    public void createHotel() throws Exception{
+//        HotelPartner hotelPartner = new HotelPartner();
+//        given(hotelPartnerServices.createHotelPartner(hotelPartner).retu
+//        mockMvc.perform(post("/hotel/new")
+//                        .contentType(MediaType.APPLICATION_JSON)
+//                        .content(JSONUtil.toJson(hotelPartner)))
+//                .andExpect(status().isCreated());
+//    }
+    @Test
+    public void shouldCreateHotel(){
+        HotelPartner hotelPartner = new HotelPartner();
+        hotelPartner.setId(15);
+        hotelPartner.setHotelName("Testing");
+        hotelPartner.setHotelLocation("TestLocation");
+        hotelPartner.setIsDeleted(false);
+
+        when(hotelPartnerRepository.save(any(HotelPartner.class))).thenReturn(hotelPartner);
+
+        HotelPartner savedHotel = hotelPartnerRepository.save(hotelPartner);
+
+        Assert.assertFalse(savedHotel.getHotelName().isEmpty());
+
+//        assertThat(savedHotel.getHotelName()).isNotNull();
+    }
 }
+   
--- src/test/java/HotelServiceTest.java
+++ src/test/java/HotelServiceTest.java
@@ -1,24 +1,31 @@
+
 import com.example.demo.DAO.HotelPartnerRepository;
 import com.example.demo.Models.HotelPartner;
 import com.example.demo.Services.HotelPartnerServices;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.*;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
+import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+// doesn’t need any dependencies from the Spring Boot container, the Mockito‘s @Mock
+@ExtendWith(MockitoExtension.class)
 public class HotelServiceTest {
-
     @Mock
     private HotelPartnerRepository hotelPartnerRepository;
 
     @InjectMocks
     private HotelPartnerServices hotelPartnerServices;
-
 
     @Before
     public void setUp(){
@@ -37,7 +44,7 @@ public class HotelServiceTest {
     @Test
     public void saveTest(){
 
-        Mockito.when(hotelPartnerRepository.save(any())).thenReturn(newHotelPartner());
+        when(hotelPartnerRepository.save(any())).thenReturn(newHotelPartner());
         hotelPartnerServices.createHotelPartner(newHotelPartner());
 
         ArgumentCaptor<HotelPartner> captureHotel = ArgumentCaptor.forClass(HotelPartner.class);
@@ -48,7 +55,38 @@ public class HotelServiceTest {
         Assert.assertEquals(Optional.ofNullable(hotel.getHotelName()),Optional.ofNullable("TestHotel"));
         Assert.assertEquals(Optional.ofNullable(hotel.getIsDeleted()),Optional.ofNullable(false));
         Assert.assertEquals(Optional.ofNullable(hotel.getHotelLocation()),Optional.ofNullable("Niceville"));
-
-
     }
+
+    @Test
+    public void updateTest(){
+
+        when(hotelPartnerRepository.save(any())).thenReturn(newHotelPartner());
+        hotelPartnerServices.createHotelPartner(newHotelPartner());
+
+        ArgumentCaptor<HotelPartner> captureHotel = ArgumentCaptor.forClass(HotelPartner.class);
+        verify(hotelPartnerRepository).save(captureHotel.capture());
+
+        HotelPartner hotel = captureHotel.getValue();
+        Assert.assertEquals(Optional.ofNullable(hotel.getId()),Optional.ofNullable(20));
+        Assert.assertEquals(Optional.ofNullable(hotel.getHotelName()),Optional.ofNullable("TestHotel"));
+        Assert.assertEquals(Optional.ofNullable(hotel.getIsDeleted()),Optional.ofNullable(false));
+        Assert.assertEquals(Optional.ofNullable(hotel.getHotelLocation()),Optional.ofNullable("Niceville"));
+    }
+
+    @Test
+    public void shouldReturnAllHotels() {
+        when(hotelPartnerRepository.findAll()).thenReturn(Collections.emptyList());
+        List<HotelPartner> theHotelList = hotelPartnerServices.getAllHotels();
+        assertTrue(theHotelList.isEmpty());
+    }
+
+    @Test
+    void getHotelByHotelId(){
+
+        Optional<HotelPartner> hotel = hotelPartnerRepository.findById(20);
+        Assert.assertEquals(Optional.ofNullable(newHotelPartner().getHotelName()), Optional.ofNullable("TestHotel"));
+        Assert.assertEquals(Optional.ofNullable(newHotelPartner().getIsDeleted()), Optional.ofNullable(false));
+        Assert.assertEquals(Optional.ofNullable(newHotelPartner().getHotelLocation()),  Optional.ofNullable("Niceville"));
+    }
+
 }
