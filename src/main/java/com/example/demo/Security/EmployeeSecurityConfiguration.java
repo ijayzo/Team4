4894d2c5264548c5d1f07c90420c06995a04c7d3
qwakumuller <richmondawuahadjei@yahoo.com>
2022-03-01 14:56:58 -0500

    Lessing Authorization

--- src/main/java/com/example/demo/Security/EmployeeSecurityConfiguration.java
+++ src/main/java/com/example/demo/Security/EmployeeSecurityConfiguration.java
@@ -52,7 +52,7 @@ public class EmployeeSecurityConfiguration extends WebSecurityConfigurerAdapter 
                 .antMatchers("/employee/createEmployee").permitAll()
                 .antMatchers("/employee").permitAll()
                 .antMatchers("/actuator/**").permitAll()
-                .antMatchers("/employee/getAllEmployees").hasAuthority(Roles.ADMIN.name()) //TODO: Restrict It From Employees
+//                .antMatchers("/employee/getAllEmployees").hasAuthority(Roles.ADMIN.name()) //TODO: Restrict It From Employees
                 .anyRequest().authenticated();
 
         http.addFilterBefore(employeeJWTRequestFilter, UsernamePasswordAuthenticationFilter.class);
